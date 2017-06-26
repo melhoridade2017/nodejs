50fdc30bbb9e74bc742a14640d179915e4912dae
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2017-06-26 20:27:38 -0300

    First Commit

--- site/index.js
+++ site/index.js
@@ -4,5 +4,8 @@ let router = express.Router();
 //Eu crio as rotas do site
 router.get('/', require('./home/index'));
 router.get('/about', require('./about/index'));
+router.get('*', function(req, res){
+  res.render('errors/404');
+});
 
 module.exports = router;
